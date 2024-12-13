cfb44ff9e6fce6583c3ddbc2f162bb79cbf948c2
Léon Spohr <l.spohr@micromata.de>
2024-12-13 16:02:24 +0100

    Created main

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import discord
+from secrets/secrets import DISCORD_BOT_TOKEN
+
+
+
+
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print(f'Logged on as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'Message from {message.author}: {message.content}')
+
+
+def start_bot():
+    intents = discord.Intents.default()
+    intents.message_content = True
+
+    client = MyClient(intents=intents)
+    client.run(DISCORD_BOT_TOKEN)
+
+    
+
+if __name__ == "__main__":
+    start_bot()
+
